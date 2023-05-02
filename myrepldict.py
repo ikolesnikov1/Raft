20aecd9140775759b4d6d952063a36e57e8a0bb3
llya <70789333+ikolesnikov1@users.noreply.github.com>
2023-05-03 04:36:55 +0700

    Update myrepldict.py

--- myrepldict.py
+++ myrepldict.py
@@ -9,11 +9,23 @@ class MyReplDict(SyncObjConsumer):
     def __init__(self):
         super(MyReplDict, self).__init__()
         self.__data = {}
+        self.__lock = {}
 
     @replicated
-    def set(self, value):
-        key = one_byte_hash(value.encode('utf-8'))
+    def set(self, key, value):
+        self.__lock[key] = True
         self.__data[key] = value
+        self.__lock[key] = False
 
     def values(self):
         return self.__data.values()
+
+    @replicated
+    def lock(self, key):
+        while self.__lock.get(key, False):
+            pass
+        self.__lock[key] = True
+
+    @replicated
+    def unlock(self, key):
+        self.__lock[key] = False
