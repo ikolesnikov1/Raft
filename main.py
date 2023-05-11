ed16977cd9a4e110c60ef7f0c43c2a06998d36d7
llya <70789333+ikolesnikov1@users.noreply.github.com>
2023-05-11 12:29:55 +0700

    Update main.py

--- main.py
+++ main.py
@@ -29,6 +29,6 @@ if __name__ == '__main__':
                 print(f"{n.id}")
             print()
 
-        dict1.lock("mutex_key", sync=True)
+        dict1.lock("mutex_key")
         dict1.set(f'testValue_{random.randint(1, 100)}')
-        dict1.unlock("mutex_key", sync=True)
+        dict1.unlock("mutex_key")
